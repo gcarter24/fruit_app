1fdbf8e0c62063104310810f5130f04e977ec70f
George <gchriscarter@gmail.com>
2021-03-30 22:10:07 -0400

    add fruit model, controller and index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# fruit = Fruit.new(name: "orange", tastiness: "10", color: "orange")
+
+# fruit.save
